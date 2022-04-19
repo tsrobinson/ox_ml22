82a614ffb9868a4dd395b006850dbcb41d2b1c1b
Thomas Robinson <thomas.robinson@durham.ac.uk>
2022-04-19 09:22:14 +0100

    minor corrections + predict_row

--- code/LogisticMLE.R
+++ code/LogisticMLE.R
@@ -18,10 +18,10 @@ X_data <- data.frame(X0 = 1, # Include a constant to model the intercept
                      X2 = runif(100,-2,2))
 
 # Calculate Y based on known functional form (incl. random error term)
-Y <- 1/(1+exp(-(3 + X_data$X1 - 2*X_data$X2 + rnorm(100,0,0.05))))
+Y <- 1/(1+exp(-(3 + 1*X_data$X1 - 2*X_data$X2 + rnorm(100,0,0.05))))
 
 # Custom function to get logistic yhat predictions
-predict <- function(row, coefficients) {
+predict_row <- function(row, coefficients) {
   pred_terms <- row*coefficients
   yhat <- sum(pred_terms)
   return(1/(1+exp(-yhat)))
@@ -31,21 +31,21 @@ predict <- function(row, coefficients) {
 
 # Take a guess at the coefficients
 coef_guess <- c(0,0.5,1)
-Yhat_guess <- apply(X_data, 1, predict, coefficients = coef_guess)
+Yhat_guess <- apply(X_data, 1, predict_row, coefficients = coef_guess)
 
 # Calculate MSE
 MSE_guess <- mean((Y - Yhat_guess)^2)
 
 # Generate "true" values for test data 
 coef_actual <- c(3,1,-2)
-Yhat_actual <- apply(X_data, 1, predict, coefficients = coef_actual)
+Yhat_actual <- apply(X_data, 1, predict_row, coefficients = coef_actual)
 
 # Calculate MSE
 MSE_actual <- mean((Y - Yhat_actual)^2)
 
 #### 3. Define a logistic training algorithm ####
 
-train <- function(X,y,l_rate, reps) {
+train <- function(X, y, l_rate, reps) {
   
   # Instantiate model with basic guess of 0 for all coefs
   coefs <- rep(0, ncol(X))
@@ -59,7 +59,7 @@ train <- function(X,y,l_rate, reps) {
       
       row_vec <- as.numeric(X[i,])
       
-      yhat_i <- predict(row_vec, coefficients = coefs)
+      yhat_i <- predict_row(row_vec, coefficients = coefs)
       error <- y[i] - yhat_i
       total_error <- total_error + error^2
       
